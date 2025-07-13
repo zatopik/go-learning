29dea753d1e47a5b5f09bf4cfcfe49124aef3c65
zatopik <almaz.android.10@gmail.com>
2025-07-13 20:26:42 +0300

    Добавил в Api горутину

--- Api/Api.go
+++ Api/Api.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 type guy struct {
@@ -13,24 +13,43 @@ type guy struct {
 }
 
 func main() {
+	chanel := make(chan (string))
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		response := map[string]string{"name": "Иван"}
 		json.NewEncoder(w).Encode(response)
 	})
-	http.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
-		answer := r.URL.Query().Get("name")
-		if answer == "" {
-			http.Error(w, `{"error":"Параметр name не найден"}`, http.StatusBadRequest)
-		}
-		_, err := fmt.Fprint(w, answer)
+	http.HandleFunc("/greet", fetchWords(chanel))
+	go func() {
+		file, err := os.OpenFile("names.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 		if err != nil {
-			http.Error(w, `{"error":"неполучилось вывести ответ"}`, http.StatusBadRequest)
+			log.Fatal("Ошибка открытия файла:", err)
 		}
-	})
+		defer file.Close()
+
+		for name := range chanel {
+			_, err := file.WriteString(name + "\n")
+			if err != nil {
+				log.Println("Ошибка записи в файл:", err)
+			}
+		}
+	}()
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("Ошибка запуска сервера:", err)
 	}
 	log.Println("Сервер запущен")
+}
+func fetchWords(ch chan<- string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Query().Get("name")
+		if name == "" {
+			http.Error(w, `{"error":"Параметр name обязателен"}`, http.StatusBadRequest)
+			return
+		}
+		ch <- name
+		json.NewEncoder(w).Encode(map[string]string{
+			"message": "Привет, " + name + "!",
+		})
+	}
 }
