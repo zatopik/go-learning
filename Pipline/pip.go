e83280d70d4ec6c48373b3cd813d22a0b92a0451
zatopik <almaz.android.10@gmail.com>
2025-07-20 11:43:38 +0300

    Добавил контекст для контроля времени работы программмы

--- Pipline/pip.go
+++ Pipline/pip.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type he struct {
@@ -18,35 +20,51 @@ type he struct {
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	number := getnumber(reader)
-	result := stepen(generatenumbers(number))
+	cx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	result := stepen(generatenumbers(number, cx), cx)
 	for i := range result {
 		fmt.Printf("Число: %d, Квадрат: %d, Куб: %d\n",
 			i.number, i.numbertwotime, i.numberthreetime)
 	}
 }
 
-func generatenumbers(number int) <-chan int {
+func generatenumbers(number int, cx context.Context) <-chan int {
 	out := make(chan (int))
 	go func() {
 		for i := 1; i <= number; i++ {
 			out <- i
 		}
-		close(out)
+		select {
+		case <-cx.Done():
+			log.Fatal("программа сликшом долго работает")
+		default:
+		}
+		defer close(out)
 	}()
 	return out
 }
-func stepen(numbers <-chan int) <-chan he {
+func stepen(numbers <-chan int, cx context.Context) <-chan he {
 	out := make(chan (he))
 	go func() {
 		for i := range numbers {
+			if cx.Err() != nil {
+				log.Println("Обработка прервана")
+				return
+			}
 			out <- he{
 				number:          i,
 				numbertwotime:   i * i,
 				numberthreetime: i * i * i,
 			}
+			select {
+			case <-cx.Done():
+				log.Fatal("программа сликшом долго работает")
+			default:
+			}
 
 		}
-		close(out)
+		defer close(out)
 	}()
 	return out
 }
